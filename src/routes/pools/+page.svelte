3de69e105a002abc66f9143c9c6dd379a9513412
webdevalgo <webdevalgo@proton.me>
2023-12-29 09:21:39 +0500

    show current liquidity in the pool

--- src/routes/pools/+page.svelte
+++ src/routes/pools/+page.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { type Token, knownTokens } from '$lib';
+	import { currentAppId } from '$lib/_deployed';
+	import { getArc200Balance, getBalance, viaAppId } from '$lib/_shared';
+	import algosdk from 'algosdk';
 
 	let pools: [Token, Token][] = [[knownTokens[0], knownTokens[1]]];
+
+	async function loadLiq(): Promise<[number, number]> {
+		const voiBalance = await getBalance(algosdk.getApplicationAddress(currentAppId));
+		const viaBalance = await getArc200Balance(viaAppId, algosdk.getApplicationAddress(currentAppId));
+		return [voiBalance, viaBalance];
+	}
 </script>
 
 <div class="p-12 flex flex-col justify-start items-stretch h-full gap-2 pt-20">
@@ -10,9 +19,13 @@
 			<div class="w-full flex flex-col lg:flex-row justify-between items-center py-2 px-4">
 				<span class="text-md">{pool[0].ticker}-{pool[1].ticker}</span>
 				<span class="text-sm">
-					Liq. {10000} VOI / {20000} VIA
+					{#await loadLiq()}
+						Liq. ... VOI / ... VIA
+					{:then [voi, via]}
+						Liq. {(voi / 1e6).toFixed(2)} VOI / {(via / 1e6).toFixed(2)} VIA
+					{/await}
 				</span>
-				<div>
+				<div class="flex">
 					<a href="/swap/{pool[0].ticker}-{pool[1].ticker}" class="btn btn-ghost btn-sm">SWAP</a>
 					<a href="/liquidity/{pool[0].ticker}-{pool[1].ticker}/add" class="btn btn-ghost btn-sm"> Add Liquidity </a>
 					<a href="/liquidity/{pool[0].ticker}-{pool[1].ticker}/remove" class="btn btn-ghost btn-sm">
