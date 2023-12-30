33911d2879302e39e8062b1f79318b91d8da18fe
webdevalgo <webdevalgo@proton.me>
2023-12-30 16:34:21 +0500

    Update admin panel

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -8,6 +8,13 @@
 	let fee = 0;
 	let admin = '';
 
+	let selection_pk: string;
+	let state_proof_pk: string;
+	let vote_pk: string;
+	let vote_first: number;
+	let vote_last: number;
+	let vote_key_dilution: number;
+
 	async function deploy() {
 		appId = (await deployVoiSwap(0)) ?? appId;
 	}
@@ -61,54 +68,104 @@
 			console.log('invalid address admin =', admin);
 		}
 	}
+
+	async function register_offline() {
+		const client = getClient(appId);
+		await client.registerOffline({});
+	}
+
+	async function register_online() {
+		const client = getClient(appId);
+		await client.registerOnline({
+			selection_pk: Uint8Array.from(Buffer.from(selection_pk, 'base64')),
+			state_proof_pk: Uint8Array.from(Buffer.from(state_proof_pk, 'base64')),
+			vote_pk: Uint8Array.from(Buffer.from(vote_pk, 'base64')),
+			vote_first: vote_first,
+			vote_last: vote_last,
+			vote_key_dilution: vote_key_dilution,
+		});
+	}
 </script>
 
-<section class="p-4 h-full flex flex-col justify-center items-center gap-3">
-	<div class="w-full max-w-[300px] flex flex-col justify-center">
-		<div>App Id:</div>
-		<input class="input input-primary" type="number" bind:value={appId} />
-	</div>
-	<div class="w-full max-w-[300px] flex flex-col justify-center">
-		<button class="btn btn-primary btn-sm" on:click={deploy}>DEPLOY</button>
-	</div>
-	{#if appId !== 0 && appId > 100}
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<button class="btn btn-primary btn-sm" on:click={update}>Update</button>
+<section class="pt-12 p-4 h-full flex flex-row justify-evenly items-center gap-3">
+	<div class="h-full flex flex-col justify-start items-center gap-3 w-full">
+		<div class="w-full max-w-[610px] flex flex-col justify-center">
+			<div>App Id:</div>
+			<input class="input input-primary" type="number" bind:value={appId} />
 		</div>
+		<div class="w-full max-w-[610px] flex flex-col justify-center">
+			<button class="btn btn-primary btn-sm" on:click={deploy}>DEPLOY</button>
+		</div>
+		{#if appId !== 0 && appId > 100}
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={update}>Update</button>
+			</div>
 
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<div>Pool Token:</div>
-			<input class="input input-primary" type="number" bind:value={poolToken} />
-		</div>
-		{#if !poolToken}
-			<div class="w-full max-w-[300px] flex flex-col justify-center">
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Pool Token:</div>
+				<input class="input input-primary" type="number" bind:value={poolToken} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
 				<button class="btn btn-primary btn-sm" on:click={create_pool_token}>Create LPT</button>
 			</div>
-		{:else}
-			<div class="w-full max-w-[300px] flex flex-col justify-center">
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
 				{#await getInitializetion()}
 					Initialized: ...
 				{:then isInitialized}
 					Initialized: {Boolean(isInitialized?.asByteArray()[0])}
 				{/await}
 			</div>
-		{/if}
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Admin:</div>
+				<input class="input input-primary" type="text" bind:value={admin} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={set_admin}>Set Admin</button>
+			</div>
 
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<div>Admin:</div>
-			<input class="input input-primary" type="text" bind:value={admin} />
-		</div>
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<button class="btn btn-primary btn-sm" on:click={set_admin}>Set Admin</button>
-		</div>
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<div>Fee:</div>
-			<input class="input input-primary" type="number" min={0} max={10000} step={1} bind:value={fee} />
-		</div>
-		<div class="w-full max-w-[300px] flex flex-col justify-center">
-			<button class="btn btn-primary btn-sm" on:click={update_fee}>Update Fee</button>
-		</div>
-	{/if}
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Fee:</div>
+				<input class="input input-primary" type="number" min={0} max={10000} step={1} bind:value={fee} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={update_fee}>Update Fee</button>
+			</div>
+		{/if}
+	</div>
+	<div class="h-full flex flex-col justify-start items-center gap-3 w-full">
+		{#if appId !== 0 && appId > 100}
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Selection PK:</div>
+				<input class="input input-primary" type="text" bind:value={selection_pk} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>State Proof PK:</div>
+				<input class="input input-primary" type="text" bind:value={state_proof_pk} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Vote PK:</div>
+				<input class="input input-primary" type="text" bind:value={vote_pk} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Vote First:</div>
+				<input class="input input-primary" type="number" bind:value={vote_first} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Vote Last:</div>
+				<input class="input input-primary" type="number" bind:value={vote_last} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Vote Key Dilution:</div>
+				<input class="input input-primary" type="number" bind:value={vote_key_dilution} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={register_online}>Register Online</button>
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={register_offline}>Register Offline</button>
+			</div>
+		{/if}
+	</div>
 </section>
 
 <style>
