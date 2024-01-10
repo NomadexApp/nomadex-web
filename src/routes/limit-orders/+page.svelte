9784140fd45726685e2dc1703c2545004a9cf821
webdevalgo <webdevalgo@proton.me>
2024-01-10 17:40:24 +0500

    loading spinner for limit order page

--- src/routes/limit-orders/+page.svelte
+++ src/routes/limit-orders/+page.svelte
@@ -18,36 +18,42 @@
 		arc200Amount: bigint;
 		isDirectionFromArc200ToAlgo: number;
 	}[] = [];
+	let loading = false;
 
 	async function fetchOrders() {
-		const { boxes: boxesNames } = await nodeClient.getApplicationBoxes(contracts.orderbookLimitOrderApp).do();
+		try {
+			const { boxes: boxesNames } = await nodeClient.getApplicationBoxes(contracts.orderbookLimitOrderApp).do();
 
-		const boxes = await Promise.all(
-			boxesNames.map((box) => nodeClient.getApplicationBoxByName(contracts.orderbookLimitOrderApp, box.name).do())
-		);
+			const boxes = await Promise.all(
+				boxesNames.map((box) => nodeClient.getApplicationBoxByName(contracts.orderbookLimitOrderApp, box.name).do())
+			);
 
-		limitOrders = boxes
-			.map((box) => {
-				const [maker, arc200Id, algoAmount, arc200Amount, isDirectionFromArc200ToAlgo] = <
-					[string, bigint, bigint, bigint, bigint]
-				>algosdk.ABITupleType.from('(address,uint64,uint64,uint256,uint8)').decode(box.value);
-				return {
-					orderId: Number('0x' + Buffer.from(box.name).toString('hex')),
-					name: box.name,
-					value: box.value,
-					maker,
-					arc200Id: Number(arc200Id),
-					arc200Token: <Token>knownTokens.find((t) => t.id === Number(arc200Id)),
-					algoAmount: Number(algoAmount),
-					arc200Amount,
-					isDirectionFromArc200ToAlgo: Number(isDirectionFromArc200ToAlgo),
-				};
-			})
-			.filter((b) => b.arc200Token);
+			limitOrders = boxes
+				.map((box) => {
+					const [maker, arc200Id, algoAmount, arc200Amount, isDirectionFromArc200ToAlgo] = <
+						[string, bigint, bigint, bigint, bigint]
+					>algosdk.ABITupleType.from('(address,uint64,uint64,uint256,uint8)').decode(box.value);
+					return {
+						orderId: Number('0x' + Buffer.from(box.name).toString('hex')),
+						name: box.name,
+						value: box.value,
+						maker,
+						arc200Id: Number(arc200Id),
+						arc200Token: <Token>knownTokens.find((t) => t.id === Number(arc200Id)),
+						algoAmount: Number(algoAmount),
+						arc200Amount,
+						isDirectionFromArc200ToAlgo: Number(isDirectionFromArc200ToAlgo),
+					};
+				})
+				.filter((b) => b.arc200Token);
+		} catch (e) {}
 	}
 
 	onMount(async () => {
-		fetchOrders();
+		loading = true;
+		fetchOrders().finally(() => {
+			loading = false;
+		});
 		const timeout = setInterval(() => fetchOrders(), 15_000);
 		return () => clearTimeout(timeout);
 	});
@@ -125,92 +131,98 @@
 				</div>
 			</div>
 		</div>
-		{#each limitOrders.sort((a, b) => b.orderId - a.orderId) as limitOrder}
-			{@const arc200TokenAmount = Number(limitOrder.arc200Amount) / limitOrder.arc200Token.unit}
-			{@const algoTokenAmouunt = Number(limitOrder.algoAmount) / 1e6}
-			<div class="w-full flex flex-col pt-4 p-4 bg-base-300 relative">
-				<div class="pool rounded-btn flex flex-col gap-2 min-w-[100px] sm:min-w-[300px] w-full max-w-[800px]">
-					<div
-						class="flex justify-between cursor-pointer select-none"
-						on:click={() => (selcetdOrder = selcetdOrder === limitOrder.orderId ? undefined : limitOrder.orderId)}
-						on:keydown
-					>
-						<span class="name mb-0 flex justify-start items-center w-full gap-5">
-							<span class="w-24">{limitOrder.maker.slice(0, 3)}...{limitOrder.maker.slice(-3)}</span>
-							<span class="w-16">{limitOrder.isDirectionFromArc200ToAlgo ? 'BUY' : 'SELL'}</span>
+		{#if loading}
+			<div class="w-full min-h-44 flex justify-center items-center">
+				<span class="loading text-primary" />
+			</div>
+		{:else}
+			{#each limitOrders.sort((a, b) => b.orderId - a.orderId) as limitOrder}
+				{@const arc200TokenAmount = Number(limitOrder.arc200Amount) / limitOrder.arc200Token.unit}
+				{@const algoTokenAmouunt = Number(limitOrder.algoAmount) / 1e6}
+				<div class="w-full flex flex-col pt-4 p-4 bg-base-300 relative">
+					<div class="pool rounded-btn flex flex-col gap-2 min-w-[100px] sm:min-w-[300px] w-full max-w-[800px]">
+						<div
+							class="flex justify-between cursor-pointer select-none"
+							on:click={() => (selcetdOrder = selcetdOrder === limitOrder.orderId ? undefined : limitOrder.orderId)}
+							on:keydown
+						>
+							<span class="name mb-0 flex justify-start items-center w-full gap-5">
+								<span class="w-24">{limitOrder.maker.slice(0, 3)}...{limitOrder.maker.slice(-3)}</span>
+								<span class="w-16">{limitOrder.isDirectionFromArc200ToAlgo ? 'BUY' : 'SELL'}</span>
 
-							<span class="w-24 text-nowrap">
-								{algoTokenAmouunt.toLocaleString('en')} VOI
-							</span>
-							<span class="hidden md:block flex-grow max-w-16" />
-							<span class="text-nowrap">
-								{(arc200TokenAmount / algoTokenAmouunt).toLocaleString('en')}
-								{limitOrder.arc200Token.ticker}
-								<span class="text-[0.7rem]">
-								<span class="hidden md:inline-block">/ VOI</span>
+								<span class="w-24 text-nowrap">
+									{algoTokenAmouunt.toLocaleString('en')} VOI
+								</span>
+								<span class="hidden md:block flex-grow max-w-16" />
+								<span class="text-nowrap">
+									{(arc200TokenAmount / algoTokenAmouunt).toLocaleString('en')}
+									{limitOrder.arc200Token.ticker}
+									<span class="text-[0.7rem]">
+										<span class="hidden md:inline-block">/ VOI</span>
+									</span>
 								</span>
 							</span>
-						</span>
-						<div class="w-12">
-							{#if limitOrder.maker === $connectedAccount}
-								<button class="btn btn-sm btn-ghost" on:click={(e) => cancelLimitOrder(e, limitOrder)}>x</button>
-							{:else}
-								&nbsp;
+							<div class="w-12">
+								{#if limitOrder.maker === $connectedAccount}
+									<button class="btn btn-sm btn-ghost" on:click={(e) => cancelLimitOrder(e, limitOrder)}>x</button>
+								{:else}
+									&nbsp;
+								{/if}
+							</div>
+						</div>
+					</div>
+					<div
+						class="pool rounded-btn flex flex-col items-center overflow-hidden gap-2 min-w-[100px] sm:min-w-[300px] w-full max-w-[800px] transition-all duration-200"
+						style="height: {selcetdOrder === Number(limitOrder.orderId) ? '200' : '0'}px;"
+					>
+						<div class="flex flex-col pt-4 gap-2 w-full max-w-[448px]">
+							<label for="">
+								Enter {limitOrder.isDirectionFromArc200ToAlgo ? 'VOI' : limitOrder.arc200Token.ticker} amount</label
+							>
+							<input
+								type="number"
+								placeholder="Enter {limitOrder.isDirectionFromArc200ToAlgo
+									? 'VOI'
+									: limitOrder.arc200Token.ticker} amount"
+								bind:value={amounts[limitOrder.orderId]}
+								step={0.000001}
+								required
+								class="input input-primary input-bordered w-full focus:outline-none"
+							/>
+							<div class="flex justify-center mt-2 pr-0">
+								<button
+									class="btn btn-primary w-full box-border"
+									disabled={!isValid(amounts[limitOrder.orderId], limitOrder)}
+									class:btn-outline={!isValid(amounts[limitOrder.orderId], limitOrder)}
+									on:click={() => sell(amounts[limitOrder.orderId], limitOrder)}
+								>
+									{#if limitOrder.isDirectionFromArc200ToAlgo}
+										SELL
+									{:else}
+										BUY
+									{/if}
+								</button>
+							</div>
+
+							{#if isValid(amounts[limitOrder.orderId], limitOrder)}
+								<div class="text-right">
+									You will receive {amountAfterFee(
+										Number(
+											amounts[limitOrder.orderId] *
+												(limitOrder.isDirectionFromArc200ToAlgo
+													? arc200TokenAmount / algoTokenAmouunt
+													: algoTokenAmouunt / arc200TokenAmount)
+										),
+										contractsConstants.orderbookLimitOrderAppFeePercent
+									).toLocaleString('en')}
+									{limitOrder.isDirectionFromArc200ToAlgo ? limitOrder.arc200Token.ticker : 'VOI'}
+								</div>
 							{/if}
 						</div>
 					</div>
 				</div>
-				<div
-					class="pool rounded-btn flex flex-col items-center overflow-hidden gap-2 min-w-[100px] sm:min-w-[300px] w-full max-w-[800px] transition-all duration-200"
-					style="height: {selcetdOrder === Number(limitOrder.orderId) ? '200' : '0'}px;"
-				>
-					<div class="flex flex-col pt-4 gap-2 w-full max-w-[448px]">
-						<label for="">
-							Enter {limitOrder.isDirectionFromArc200ToAlgo ? 'VOI' : limitOrder.arc200Token.ticker} amount</label
-						>
-						<input
-							type="number"
-							placeholder="Enter {limitOrder.isDirectionFromArc200ToAlgo
-								? 'VOI'
-								: limitOrder.arc200Token.ticker} amount"
-							bind:value={amounts[limitOrder.orderId]}
-							step={0.000001}
-							required
-							class="input input-primary input-bordered w-full focus:outline-none"
-						/>
-						<div class="flex justify-center mt-2 pr-0">
-							<button
-								class="btn btn-primary w-full box-border"
-								disabled={!isValid(amounts[limitOrder.orderId], limitOrder)}
-								class:btn-outline={!isValid(amounts[limitOrder.orderId], limitOrder)}
-								on:click={() => sell(amounts[limitOrder.orderId], limitOrder)}
-							>
-								{#if limitOrder.isDirectionFromArc200ToAlgo}
-									SELL
-								{:else}
-									BUY
-								{/if}
-							</button>
-						</div>
-
-						{#if isValid(amounts[limitOrder.orderId], limitOrder)}
-							<div class="text-right">
-								You will receive {amountAfterFee(
-									Number(
-										amounts[limitOrder.orderId] *
-											(limitOrder.isDirectionFromArc200ToAlgo
-												? arc200TokenAmount / algoTokenAmouunt
-												: algoTokenAmouunt / arc200TokenAmount)
-									),
-									contractsConstants.orderbookLimitOrderAppFeePercent
-								).toLocaleString('en')}
-								{limitOrder.isDirectionFromArc200ToAlgo ? limitOrder.arc200Token.ticker : 'VOI'}
-							</div>
-						{/if}
-					</div>
-				</div>
-			</div>
-		{/each}
+			{/each}
+		{/if}
 	</div>
 </section>
 
