58f3dd20379ff8011187584b5b544ac8f9bc811b
webdevalgo <webdevalgo@proton.me>
2024-01-26 13:23:21 +0500

    add way to withdraw fee from limit order contract

--- src/routes/admin/limit-order/+page.svelte
+++ src/routes/admin/limit-order/+page.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
-	import { getTransactionSignerAccount } from '$lib/UseWallet.svelte';
+	import { connectedAccount, getTransactionSignerAccount } from '$lib/UseWallet.svelte';
 	import { nodeClient } from '$lib/_shared';
 	import algosdk from 'algosdk';
 	import { LimitOrders001Client } from '../../../contracts/clients/LimitOrders001Client';
 	import { onMount } from 'svelte';
 	import { AlgoAmount } from '@algorandfoundation/algokit-utils/types/amount';
 	import { contracts } from '$lib';
+	import { LimitOrders001ClientConnector } from '$lib/LimitOrderConnector';
 
 	let appId = contracts.orderbookLimitOrderApp;
 	let feePercent = 1;
 	let owner = 'DYX2V5XF4IKOHE55Z63XAHVBJTMYM723HK5WJZ72BDZ5AFEFKJ5YP4DOQQ';
 	const SCALE = 100_000_000_000_000;
+
+	let withdrawFeeAmount = 0;
 
 	let selection_pk = '';
 	let state_proof_pk = '';
@@ -110,63 +113,80 @@
 
 		await client.registerOffline({});
 	}
+
+	async function withdrawFee() {
+		const amount = algosdk.algosToMicroalgos(withdrawFeeAmount);
+		const signer = getTransactionSignerAccount();
+		console.log(amount);
+		const client = new LimitOrders001ClientConnector(appId, signer);
+		await client.invoke('collectFee', { algoAmount: amount });
+	}
 </script>
 
-<section class="pt-12 p-4 h-full flex flex-row justify-evenly items-center gap-3">
-	<div class="h-full flex flex-col justify-start items-center gap-3 w-full">
-		<div class="w-full max-w-[610px] flex flex-col justify-center">
-			<div>App Id:</div>
-			<input class="input input-primary" type="number" bind:value={appId} />
-		</div>
-		<div class="w-full max-w-[610px] flex flex-col justify-center">
-			<div>Owner:</div>
-			<input class="input input-primary" type="text" bind:value={owner} />
-		</div>
-		<div class="w-full max-w-[610px] flex flex-col justify-center">
-			<div>Fee Percent:</div>
-			<input class="input input-primary" type="number" min={0} max={100} bind:value={feePercent} />
-		</div>
-		{#if !appId}
+{#if owner === $connectedAccount}
+	<section class="pt-12 p-4 h-full flex flex-row justify-evenly items-center gap-3">
+		<div class="h-full flex flex-col justify-start items-center gap-3 w-full">
 			<div class="w-full max-w-[610px] flex flex-col justify-center">
-				<button class="btn btn-primary btn-sm" on:click={deployLimitOrders}>DEPLOY</button>
+				<div>App Id:</div>
+				<input class="input input-primary" type="number" bind:value={appId} />
 			</div>
-		{:else}
-			<br />
-			{#if appId !== 0 && appId > 100}
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Owner:</div>
+				<input class="input input-primary" type="text" bind:value={owner} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Fee Percent:</div>
+				<input class="input input-primary" type="number" min={0} max={100} bind:value={feePercent} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<div>Collect Fee:</div>
+				<input class="input input-primary" type="number" min={0} step={0.000001} bind:value={withdrawFeeAmount} />
+			</div>
+			<div class="w-full max-w-[610px] flex flex-col justify-center">
+				<button class="btn btn-primary btn-sm" on:click={withdrawFee}>Collect Now</button>
+			</div>
+			{#if !appId}
 				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>Selection PK:</div>
-					<input class="input input-primary" type="text" bind:value={selection_pk} />
+					<button class="btn btn-primary btn-sm" on:click={deployLimitOrders}>DEPLOY</button>
 				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>State Proof PK:</div>
-					<input class="input input-primary" type="text" bind:value={state_proof_pk} />
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>Vote PK:</div>
-					<input class="input input-primary" type="text" bind:value={vote_pk} />
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>Vote First:</div>
-					<input class="input input-primary" type="number" bind:value={vote_first} />
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>Vote Last:</div>
-					<input class="input input-primary" type="number" bind:value={vote_last} />
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<div>Vote Key Dilution:</div>
-					<input class="input input-primary" type="number" bind:value={vote_key_dilution} />
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<button class="btn btn-primary btn-sm" on:click={registerOnline}>Register Online</button>
-				</div>
-				<div class="w-full max-w-[610px] flex flex-col justify-center">
-					<button class="btn btn-primary btn-sm" on:click={registerOffline}>Register Offline</button>
-				</div>
+			{:else}
+				<br />
+				{#if appId !== 0 && appId > 100}
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>Selection PK:</div>
+						<input class="input input-primary" type="text" bind:value={selection_pk} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>State Proof PK:</div>
+						<input class="input input-primary" type="text" bind:value={state_proof_pk} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>Vote PK:</div>
+						<input class="input input-primary" type="text" bind:value={vote_pk} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>Vote First:</div>
+						<input class="input input-primary" type="number" bind:value={vote_first} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>Vote Last:</div>
+						<input class="input input-primary" type="number" bind:value={vote_last} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<div>Vote Key Dilution:</div>
+						<input class="input input-primary" type="number" bind:value={vote_key_dilution} />
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<button class="btn btn-primary btn-sm" on:click={registerOnline}>Register Online</button>
+					</div>
+					<div class="w-full max-w-[610px] flex flex-col justify-center">
+						<button class="btn btn-primary btn-sm" on:click={registerOffline}>Register Offline</button>
+					</div>
+				{/if}
 			{/if}
-		{/if}
-	</div>
-</section>
+		</div>
+	</section>
+{/if}
 
 <style>
 </style>
