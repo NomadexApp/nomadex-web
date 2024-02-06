e7bf4be7599a7b38a12af4f0b3521d2496aabef4
webdevalgo <webdevalgo@proton.me>
2024-02-06 15:19:16 +0500

    Update logo

--- src/lib/Sidebar.svelte
+++ src/lib/Sidebar.svelte
@@ -3,20 +3,19 @@
 	import { goto } from '$app/navigation';
 	import UseWallet from './UseWallet.svelte';
 	import SwapIcon from 'svelte-star/dist/md/MdAutorenew.svelte';
-	import PoolsIcon from 'svelte-star/dist/md/MdFormatListBulleted.svelte';
 	import AnalyticsIcon from 'svelte-star/dist/md/MdShowChart.svelte';
 	import MdPeopleOutline from 'svelte-star/dist/md/MdPeopleOutline.svelte';
 	import MdMenu from 'svelte-star/dist/md/MdMenu.svelte';
 	import { isDarkTheme, lastActiveAnalyticsPair, lastActiveSwapPair } from './stores';
 	import { onMount } from 'svelte';
-	import { knownPools, knownTokens } from '$lib';
-	import { pageContentRefresh } from './utils';
+	import { knownPools } from '$lib';
 	import { page } from '$app/stores';
 	import MdToll from 'svelte-star/dist/md/MdToll.svelte';
 	import MdAddCircle from 'svelte-star/dist/md/MdToll.svelte';
 	import IoMdSwap from 'svelte-star/dist/io/IoMdSwap.svelte';
 	import MdLaunch from 'svelte-star/dist/md/MdLaunch.svelte';
 	import { getLastActivePair } from './config';
+	import Logo from './Logo.svelte';
 
 	let sidebarWidth = 0;
 	let innerWidth = browser ? window.innerWidth : 0;
@@ -65,7 +64,9 @@
 				on:click={() => isMobile && (sidebarOpen = false)}
 				on:keydown
 			>
-				<img class="grayscale" alt="voi logo" src="/logo.png" />
+				<div class="w-24">
+					<Logo />
+				</div>
 			</div>
 			{#if $knownPools.length}
 				<li class="pl-0 sm:block" class:active={$page.url.pathname === '/'}>
