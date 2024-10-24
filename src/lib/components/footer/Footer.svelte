76f11217fbe39abc49479cd29d0f711f031b3256
webdev.algo <webdevalgo@gmail.com>
2024-10-24 10:11:38 +0500

    update footer

--- src/lib/components/footer/Footer.svelte
+++ src/lib/components/footer/Footer.svelte
@@ -9,7 +9,7 @@
 
 <svelte:window bind:scrollY on:scroll|preventDefault />
 
-<footer class="" class:scrolled={fullScroll}>
+<footer class="backdrop-blur-md" class:scrolled={fullScroll}>
 	<div class="flex-col sm:flex-row max-w-screen-xl mx-auto">
 		<div class="flex justify-start gap-4">
 			<div class="w-10 text-primary">
@@ -17,13 +17,13 @@
 			</div>
 			<div class="select-none cursor-default">
 				<h2 class="text-lg font-normal">Nomadex</h2>
-				<h3 class="text-gray-400 mt-[-4px]">Decentralized Exchange</h3>
+				<h3 class="text-gray-300 mt-[-4px]">Decentralized Exchange</h3>
 			</div>
 		</div>
 		<div class="flex justify-between w-screen max-w-[280px] px-4">
 			<div class="">
 				<h3 class="mb-2">App</h3>
-				<ul class="font-thin text-gray-400">
+				<ul class="font-thin text-gray-300">
 					<li>
 						<a href="/">Swap</a>
 					</li>
@@ -40,7 +40,7 @@
 			</div>
 			<div class="">
 				<h3 class="mb-2">Links</h3>
-				<ul class="font-thin text-gray-400">
+				<ul class="font-thin text-gray-300">
 					<li><a target="_blank" href="https://twitter.com/nomadex_app">Twitter</a></li>
 					<li><a target="_blank" href="https://discord.gg/2h3YnM6Y">Discord</a></li>
 					<li><a target="_blank" href="https://github.com/NomadexApp">Github</a></li>
@@ -52,9 +52,18 @@
 
 <style>
 	footer {
-		background: #222222;
+		background: #22222288;
 		min-height: 200px;
 		padding: 2rem 2rem;
+		position: relative;
+	}
+	footer::after {
+		content: "";
+		position: fixed;
+		left: 0;
+		right: 0;
+		top: 90vh;
+		bottom: -200px;
 	}
 	footer > div {
 		display: flex;
