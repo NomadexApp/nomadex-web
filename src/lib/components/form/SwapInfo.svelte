8c86b31ad9488615ffdd38b45dfe6094bddbdb03
webdev.algo <webdevalgo@gmail.com>
2024-11-03 14:05:20 +0500

    Add color indicator to donate high price impact

--- src/lib/components/form/SwapInfo.svelte
+++ src/lib/components/form/SwapInfo.svelte
@@ -4,9 +4,15 @@
 
 <div class="info text-gray-300 text-sm flex flex-col">
 	{#each data as info}
+		{@const impact = Number(info[1].slice(0, -1))}
 		<div class="">
 			<div class="">{info[0]}</div>
-			<div class="text-right">{info[1]}</div>
+			<div class="text-right">
+				{#if info[0] === 'Price impact' && impact > 5}
+					<span style="color: hsl({Math.min(40, Math.max(10, 40 - impact))} 100% 50%);">[expensive]</span>
+				{/if}
+				{info[1]}
+			</div>
 		</div>
 	{/each}
 </div>
