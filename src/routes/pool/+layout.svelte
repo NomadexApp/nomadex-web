6a99e1a4fa00e4e1c5909febd6e8e22f7ac2e785
webdev.algo <webdevalgo@gmail.com>
2024-11-05 07:54:43 +0500

    change APY to APR

--- src/routes/pool/+layout.svelte
+++ src/routes/pool/+layout.svelte
@@ -102,7 +102,7 @@
 				<div>Name</div>
 				<div>{my ? 'Value' : 'TVL'}</div>
 				<div class="inline-flex items-start">VOL<span class="text-xs -mt-1 text-gray-300">7d</span></div>
-				<div>APY</div>
+				<div>APR</div>
 				<div>&nbsp;</div>
 			</div>
 
