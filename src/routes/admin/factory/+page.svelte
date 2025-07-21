45c115765b2a9a4b05761a99fa4ca2ba1bf9a03b
webdevalgo <webdevalgo@gmail.com>
2025-07-21 12:43:01 +0500

    update

--- src/routes/admin/factory/+page.svelte
+++ src/routes/admin/factory/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import algosdk from 'algosdk';
+	import algosdk, { AtomicTransactionComposer, makeEmptyTransactionSigner } from 'algosdk';
 	import { connectedAccount, getTransactionSignerAccount } from '$lib/components/UseWallet.svelte';
 	import { nodeClient } from '$lib/_shared';
 	import { PoolFactoryClient } from '../../../contracts/clients/PoolFactoryClient';
@@ -8,7 +8,8 @@
 	import { PUBLIC_NETWORK } from '$env/static/public';
 	import { onMount } from 'svelte';
 	import { convertDecimals } from '$lib/utils/numbers';
-	import { TokenType } from 'nomadex-client';
+	import { MySmartAsset, TokenType } from 'nomadex-client';
+	import { populateAppCallResources } from '@algorandfoundation/algokit-utils';
 
 	let appId = contracts[PUBLIC_NETWORK].poolFcatory;
 	let feePercent = 1;
@@ -133,6 +134,102 @@
 		);
 		console.log(resp);
 	}
+
+	async function withdrawAll() {
+		const factoryAddress = algosdk.getApplicationAddress(appId);
+		const info = await nodeClient.accountInformation(factoryAddress).do();
+		const txns: algosdk.Transaction[] = [];
+		console.log(info);
+
+		const signer = getTransactionSignerAccount();
+		const client = new PoolFactoryClient(
+			{
+				resolveBy: 'id',
+				id: appId,
+				sender: signer,
+			},
+			nodeClient
+		);
+
+		for (const token of $knownTokens) {
+			if (txns.length >= 16) continue;
+			if (token.type === 0) {
+				const available = Number(info.amount) - Number(info['min-balance']) - 100_000_000;
+				if (available <= 0) continue;
+				console.log(token.symbol, available / 10 ** token.decimals);
+				const atc = await client
+					.compose()
+					.withdraw({
+						id: [0, 0],
+						amount: available,
+						to: feeWithdrawTo,
+					})
+					.atc();
+				txns.push(
+					...atc.buildGroup().map((t) => {
+						t.txn.group = undefined;
+						return t.txn;
+					})
+				);
+			} else if (token.type === 1) {
+				const asset = info.assets.find((asset) => asset['asset-id'] === token.id);
+				if (!asset) continue;
+				const available = Number(asset['amount']);
+				if (available <= 0) continue;
+				console.log(token.symbol, available / 10 ** token.decimals);
+				const atc = await client
+					.compose()
+					.withdraw({
+						id: [token.id, 0],
+						amount: available,
+						to: feeWithdrawTo,
+					})
+					.atc();
+				txns.push(
+					...atc.buildGroup().map((t) => {
+						t.txn.group = undefined;
+						return t.txn;
+					})
+				);
+			} else if (token.type === 2) {
+				const asset = MySmartAsset.from(token.id, nodeClient);
+				const available = await asset.arc200BalanceOf(factoryAddress);
+				if (available <= 0n) continue;
+				const amount = convertDecimals(available, token.decimals, 6);
+				console.log(token.symbol, Number(amount) / 10 ** 6);
+				const atc = await client
+					.compose()
+					.withdraw({
+						id: [0, token.id],
+						amount: available,
+						to: feeWithdrawTo,
+					})
+					.atc();
+				txns.push(
+					...atc.buildGroup().map((t) => {
+						t.txn.group = undefined;
+						return t.txn;
+					})
+				);
+			}
+		}
+
+		let atc = new AtomicTransactionComposer();
+		for (const txn of txns.slice(0, 16)) {
+			txn.group = undefined;
+			atc.addTransaction({ txn, signer: makeEmptyTransactionSigner() });
+		}
+		atc = await populateAppCallResources(atc, nodeClient);
+		const signedTxns = await signer.signer(
+			atc.buildGroup().map((t) => t.txn),
+			txns.map((_, i) => i)
+		);
+
+		const resp = await nodeClient.sendRawTransaction(signedTxns).do();
+		await algosdk.waitForConfirmation(nodeClient, resp.txId, 4);
+		console.log(resp.txId);
+		window.location.reload();
+	}
 </script>
 
 {#if owner === $connectedAccount}
@@ -187,6 +284,9 @@
 						<button class="btn btn-primary btn-sm" on:click={withdrawFee}>Withdraw Fee</button>
 					</div>
 				{/if}
+				<div class="w-full max-w-[610px] flex flex-col justify-center">
+					<button class="btn btn-primary btn-sm" on:click={withdrawAll}>Withdraw All</button>
+				</div>
 			{/if}
 		</div>
 	</section>
