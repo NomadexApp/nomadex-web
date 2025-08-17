e865f56ec39901022525a0a34a3e1a5cdf15a1fc
webdevalgo <webdevalgo@gmail.com>
2025-08-17 19:24:00 +0500

    CORN-ASA

--- src/contracts/SmartAsset.algo.ts
+++ src/contracts/SmartAsset.algo.ts
@@ -36,4 +36,49 @@ export class SmartAsset extends Contract.extend(Arc200, Ownable) {
       value: this.totalSupply.value
     });
   }
+
+  private asa_id(): AssetID {
+    return AssetID.fromUint64(40266686);
+  }
+
+  arc200_exchange(): [uint64, Address] {
+    return [this.asa_id().id, this.app.address];
+  }
+
+  post_update(): void {
+    assert(this.txn.sender === this.warden.value, "manager only");
+    sendAssetTransfer({
+      xferAsset: this.asa_id(),
+      assetSender: this.app.address,
+      assetReceiver: this.app.address,
+      assetAmount: 0,
+      fee: 0
+    });
+  }
+
+  // Redeem ASA for ARC-200
+  arc200_redeem(amount: uint64): void {
+    assert(this.txn.groupIndex > 0, "invalid group");
+    verifyAssetTransferTxn(this.txnGroup[this.txn.groupIndex - 1], {
+      xferAsset: this.asa_id(),
+      assetSender: this.txn.sender,
+      assetReceiver: this.app.address,
+      assetAmount: { greaterThanEqualTo: amount },
+      assetCloseTo: globals.zeroAddress,
+      rekeyTo: globals.zeroAddress
+    });
+    this.transfer(this.app.address, this.txn.sender, amount as uint256);
+  }
+
+  // Swap ARC-200 back to ASA
+  arc200_swapBack(amount: uint64): void {
+    this.transfer(this.txn.sender, this.app.address, amount as uint256);
+    sendAssetTransfer({
+      xferAsset: this.asa_id(),
+      assetSender: this.app.address,
+      assetReceiver: this.txn.sender,
+      assetAmount: amount,
+      fee: 0
+    });
+  }
 }
